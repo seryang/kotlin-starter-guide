08e0788805c1ee3c5fbcf689e96693d251a613f4
최세량 <seryang.choe@navercorp.com>
2023-02-27 17:39:15 +0900

    Lec14. 코틀린에서 다양한 클래스를 다루는 방법

---
+++ section3/lec14/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    // Lec14. 코틀린에서 다양한 클래스를 다루는 방법
+    // 1. Data Class (DTO - 계층간의 데이터를 전달하기 위함 Data Transfer Object)
+
+
+    // 2. Enum Class
+
+
+    // 3. Sealed Class, Sealed Interface
+
+}
